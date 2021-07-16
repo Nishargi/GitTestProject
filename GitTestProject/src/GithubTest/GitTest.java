630284fd7d5422c4d7583dcafa04c6c5a9d520db
Nishargi <nishargishah169@gmail.com>
2021-07-16 18:56:49 +0530

    First Commit

---
+++ GitTestProject/src/GithubTest/GitTest.java
@@ -0,0 +1,17 @@
+package GithubTest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String code="SE";
+		if(code=="SE") {
+			System.out.println("Software Engineer");
+		}
+		else {
+			System.out.println("Not a Software Engineer");
+		}
+
+	}
+
+}
